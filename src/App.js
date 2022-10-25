c47ffbbd9e71a033921e59e93e882f23c3c6e094
shipatulhasan <shipatulhasan328@gmail.com>
2022-10-25 15:18:17 +0600

    initialization

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import { RouterProvider } from 'react-router-dom'
+import logo from './asset/brand/logo.png'
+
+function App() {
+
+  return (
+    <div className='min-h-screen flex items-center justify-center'>
+    
+    {/* <h2 className="text-4xl">Hello bubu From Campus</h2> */}
+    <img src={logo} alt="" />
+    
+    </div>
+
+  )
+}
+
+export default App
