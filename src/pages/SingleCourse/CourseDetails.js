81c28b7fd977f6fef2b66a239b28cecb7287d999
shipatulhasan <shipatulhasan328@gmail.com>
2022-10-27 22:23:04 +0600

    adding pdf downloader

--- src/pages/SingleCourse/CourseDetails.js
+++ src/pages/SingleCourse/CourseDetails.js
@@ -8,16 +8,16 @@ const CourseDetails = ({course}) => {
     <div className="p-6">
         <div>
             <h2  className="block mt-2 text-2xl font-semibold text-gray-800 transition-colors duration-300 transform" tabindex="0" role="link">{name}</h2>
-            <h2  className="block mt-5 text-2xl font-bold text-gray-800 transition-colors duration-300 transform" tabindex="0" role="link">What you'll learn</h2>
+            <h2  className="block mt-5 text-2xl font-bold text-gray-800 transition-colors duration-300 transform">What you'll learn</h2>
             {
-                description.benefits.map(benefit=> <div className="flex  items-start mt-4 text-gray-700 dark:text-gray-200">
+                description.benefits.map((benefit,i)=> <div className="flex  items-start mt-4 text-gray-700 dark:text-gray-200" key={i}>
                 <FaCheck className="text-sm w-1/12 md:w-auto" />
       
                 <p className="px-2 w-11/12 text-sm font-bold break-words">{benefit}</p>
               </div>)
             }
              
-            <h2  className="block mt-5 text-2xl font-bold text-gray-800 transition-colors duration-300 transform" tabindex="0" role="link">Description</h2>
+            <h2  className="block mt-5 text-2xl font-bold text-gray-800 transition-colors duration-300 transform" >Description</h2>
             <p className="mt-2 text-base text-gray-600 dark:text-gray-400">{description.overview}</p>
         </div>
 
--- src/pages/SingleCourse/SingleCourse.js
+++ src/pages/SingleCourse/SingleCourse.js
@@ -3,6 +3,8 @@ import { useLoaderData } from 'react-router-dom';
 import CourseDetails from './CourseDetails';
 import Pricing from './Pricing';
 
+const ref = React.createRef();
+
 
 const SingleCourse = () => {
     const course = useLoaderData()
@@ -10,11 +12,12 @@ const SingleCourse = () => {
     return (
         <div  className="px-2 pb-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl  md:px-8">
             <img className="object-cover w-full h-96 rounded-xl" src={img}alt="Article"/>
-      <div className="grid gap-5 md:grid-cols-2 lg:grid-cols-4">
+      <div className="grid gap-5 md:grid-cols-2 lg:grid-cols-4" ref={ref}>
            <CourseDetails course={course} />
-           <Pricing course={course} />
+           <Pricing myref={ref} course={course} />
            
         </div>
+        
     </div>
     );
 };
