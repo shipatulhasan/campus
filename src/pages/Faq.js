10a72e1831dbd209e4f7ac83775cdff38966064f
shipatulhasan <shipatulhasan328@gmail.com>
2022-10-27 22:54:37 +0600

    adding faq image

--- src/pages/Faq.js
+++ src/pages/Faq.js
@@ -1,8 +1,16 @@
 import React from 'react';
+import img from '../asset/Faq.png'
 
 const Faq = () => {
     return (
-        <div className="px-4 py-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-20">
+        <div>
+            <img
+                src={img}
+                className=" object-cover w-full h-[70vh]"
+                alt=""
+            />
+
+<div className="px-4 py-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-20">
         <div className="max-w-xl sm:mx-auto lg:max-w-2xl">
           <div className="max-w-xl mb-10 md:mx-auto sm:text-center lg:max-w-2xl md:mb-12">
            
@@ -56,6 +64,8 @@ const Faq = () => {
           </div>
         </div>
       </div>
+        </div>
+        
     );
 };
 
